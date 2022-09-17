768429dc02381cb35c54b116be0b81be10ffbdcc
Kurt Stemmle <ksteme@gmail.com>
2022-09-17 11:07:13 -0400

    fix compile issue

--- decamp-react/src/components/member_pool_list.js
+++ decamp-react/src/components/member_pool_list.js
@@ -5,7 +5,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link, useNavigate } from 'react-router-dom';
-import CampaignListItem from "./campaign_list_item";
 import detectEthereumProvider from '@metamask/detect-provider';
 import Button from 'react-bootstrap/Button';
 
